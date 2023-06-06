07b0cbe12558c8939e60068a27cc18762efc699f
Nicolas Pantano <nicolaspantano756@gmail.com>
2023-06-06 10:47:00 -0300

    mongoose moved to index

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const port = process.env.port || 3000
 const {readdirSync} = require("fs")
 
 //app.use("/", require('mongoose'));
-require('./mongoose');
+//require('./mongoose');
+
+const mongoose = require('mongoose');
+
+const user = 'new_user_1';
+const password = 'pnH4hlnpa2GwyoHv';
+const databaseName = 'sample_airbnb';
+const uri = `mongodb+srv://${user}:${password}@cluster0.3aio0bs.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+mongoose.connect(uri, 
+  {
+    useNewUrlParser: true, useUnifiedTopology: true
+  }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log(e))
+
 
 //agrega todas las rutas
 readdirSync("./routes").map((file) => app.use("/", require("./routes/" + file)))
